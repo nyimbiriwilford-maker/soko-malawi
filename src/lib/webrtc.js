fe9a94bdcf36b7655c5347f89957815bc4f954fe
Wilford Nyimbiri <nyimbiriwilford@gmail.com>
2026-05-26 22:36:53 +0200

    Add Metered TURN credentials

--- src/lib/webrtc.js
+++ src/lib/webrtc.js
@@ -3,19 +3,19 @@ export const ICE_SERVERS = {
     { urls: 'stun:stun.l.google.com:19302' },
     { urls: 'stun:stun1.l.google.com:19302' },
     {
-      urls: 'turn:openrelay.metered.ca:80',
-      username: 'openrelayproject',
-      credential: 'openrelayproject'
+      urls: 'turn:a.relay.metered.ca:80',
+      username: 'ecfd28a5fc18b0f7cd190198',
+      credential: 'd+nP5nZZF5ErLEx7'
     },
     {
-      urls: 'turn:openrelay.metered.ca:443',
-      username: 'openrelayproject',
-      credential: 'openrelayproject'
+      urls: 'turn:a.relay.metered.ca:443',
+      username: 'ecfd28a5fc18b0f7cd190198',
+      credential: 'd+nP5nZZF5ErLEx7'
     },
     {
-      urls: 'turn:openrelay.metered.ca:443?transport=tcp',
-      username: 'openrelayproject',
-      credential: 'openrelayproject'
+      urls: 'turns:a.relay.metered.ca:443?transport=tcp',
+      username: 'ecfd28a5fc18b0f7cd190198',
+      credential: 'd+nP5nZZF5ErLEx7'
     }
   ]
 }
